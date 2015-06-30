06d0453a91b5a95f59b32e28d703b5af23db7672
Sven Bunge <sven.bunge@holisticon.de>
2015-06-30 21:51:47 +0200

    Changes for maven 3 and remove the apache license because the project is licensed under eclipse ...

--- src/main/java/launch4j/Launch4jMojo.java
+++ src/main/java/launch4j/Launch4jMojo.java
@@ -1,21 +1,5 @@
 package launch4j;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -32,14 +16,14 @@ import org.apache.maven.plugin.logging.Log;
 /**
  * Goal which touches a timestamp file.
  * 
- * @goal exe
+ * @goal launch4j
  * @phase package
  */
 public class Launch4jMojo extends AbstractMojo {
 	/**
 	 * Location of the file.
 	 * 
-	 * @parameter expression="${project.build.finalName}"
+	 * @parameter property="project.build.finalName"
 	 * @required
 	 * 
 	 */
@@ -48,29 +32,28 @@ public class Launch4jMojo extends AbstractMojo {
 	/**
 	 * Location of the file.
 	 * 
-	 * @parameter expression="${project.build.directory}"
+	 * @parameter property="project.build.directory"
 	 * @required
 	 * 
 	 */
 	private File outputDirectory;
 
 	/**
-	 * @parameter expression="${exe.configs}"
+	 * @parameter property="launch4j.configs"
 	 * @required
 	 * 
 	 */
 	private File[] configs;
 
 	/**
-	 * @parameter expression="${exe.launch4jHome}"
+	 * @parameter property="launch4j.launch4jHome"
 	 * @required
 	 * 
 	 */
 	private File launch4jHome;
 
 	/**
-	 * @parameter expression="${project.runtimeClasspathElements}"
-	 * 
+	 * @parameter property="project.runtimeClasspathElements"
 	 * 
 	 */
 	private List<String> classpath;
@@ -108,9 +91,7 @@ public class Launch4jMojo extends AbstractMojo {
 				getLog().info(sc.nextLine());
 			}
 		} finally {
-			if (sc != null) {
-				sc.close();
-			}
+			sc.close();
 		}
 	}
 
